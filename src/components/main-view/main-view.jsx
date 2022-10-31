2c7201b5d2207cb15a3d81f6e55578cf14948c88
Angela Lingan Tan <angelalingan@gmail.com>
2022-10-31 09:33:25 -0500

    removed the message "the list is empty"
    The reason for this change is that the movies array will initially be empty, and it will remain empty while the fetching process takes place in the background. Until the data is finally fetched, you don't want to display the message “The list is empty” since you don’t yet know whether the fetched array will be empty or not.

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -88,7 +88,7 @@ export class MainView extends React.Component {
 
         //if (selectedMovie) return <MovieView movie={selectedMovie} />; 
 
-        if (movies.length === 0) return <div className="main-view">The list is empty!</div>;
+        if (movies.length === 0) return <div className="main-view" />; //<div className="main-view">The list is empty!</div>;
 
         return (
             <div className="main-view">
