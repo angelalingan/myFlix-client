9fe1beed0a228c08d78c3ee83cb08aaaa5e0e0c3
Angela Lingan Tan <angelalingan@gmail.com>
2022-11-05 23:05:19 -0500

    added code for director-view

---
+++ src/components/director-view/director-view.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Button, Container, Col, Row } from "react-bootstrap";
+import { MovieCard } from "../movie-card/movie-card";
+import { Link } from "react-router-dom";
+
+export class DirectorView extends React.Component {
+
+}
