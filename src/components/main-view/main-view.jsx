df8f177c27fb49d321f8cc3cf102539ada86d515
Angela Lingan Tan <angelalingan@gmail.com>
2022-11-05 23:02:51 -0500

    added route path

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import axios from 'axios'; //importing axios library to fetch movies from database
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
@@ -9,6 +9,8 @@ import { RegistrationView } from '../registration-view/registration-view';
 import { LoginView } from '../login-view/login-view';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
+import { GenreView } from '../genre-view/genre-view';
+import { DirectorView } from '../director-view/director-view';
 
 export class MainView extends React.Component {
 
@@ -106,11 +108,15 @@ export class MainView extends React.Component {
 
         if (!register) return (<RegistrationView onRegistration={(register) => this.onRegistration(register)} />);
 
-        if (!user) return <LoginView onLoggedIn={user => this.onLoggedIn(user)} />;
-
+        if (!user) return <Row>
+            <Col>
+                <LoginView onLoggedIn={user => this.onLoggedIn(user)} />
+            </Col>
+        </Row>
         if (movies.length === 0) return <div className="main-view" />;
 
         return (
+            /*
             <div className="main-view">
                 {selectedMovie
                     ? (
@@ -132,6 +138,36 @@ export class MainView extends React.Component {
                 }
                 {<button onClick={() => { this.onLoggedOut() }}>Logout</button>}
             </div>
+            */
+            <Router>
+                <Row className="main-view justify-content-md-center">
+                    <Route exact path="/" render={() => {
+                        return movies.map(m => (
+                            <Col md={3} key={m._id}>
+                                <MovieCard movie={m} />
+                            </Col>
+                        ))
+                    }} />
+                    <Route path="/movies/:movieId" render={({ match, history }) => {
+                        return <Col md={8}>
+                            <MovieView movie={movies.find(m => m._id === match.params.movieId)} onBackClick={() => history.goBack()} />
+                        </Col>
+                    }} />
+                    <Route path="/directors/:name" render={({ match, history }) => {
+                        if (movies.length === 0) return <div className="main-view" />;
+                        return <Col md={8}>
+                            <DirectorView director={movies.find(m => m.Director.Name === match.params.name).Director} onBackClick={() => history.goBack()} />
+                        </Col>
+                    }} />
+                    <Route path="/genres/:name" render={({ match, history }) => {
+                        if (movies.length === 0) return <div className="main-view" />;
+                        return <Col md={8}>
+                            <GenreView genre={movies.find(m => m.Genre.Name === match.params.name).Genre} onBackClick={() => history.goBack()} />
+                        </Col>
+                    }} />
+
+                </Row>
+            </Router>
         );
 
     }
